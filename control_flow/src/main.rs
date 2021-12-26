94374fd6b92896beed0d55248e95c6917e40e836
Kyle Moad <kyle.moad@gmail.com>
2021-12-25 21:44:04 -0800

    control flow lesson and some free code funcs

---
+++ control_flow/src/main.rs
@@ -0,0 +1,115 @@
+fn main() {
+    let number = 3;
+
+    // The condition expression MUST evaluate to a bool
+    // No truthy inference
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let number = 6;
+    
+    if number % 4 == 0 {
+        println!("divisible by 4");
+    } else if number % 3 == 0 {
+        println!("divisible by 3");
+    } else if number % 2 == 0 {
+        println!("divisible by 2");
+    } else {
+        println!("not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("number: {}", number);
+    // Expressions must be of same type though
+    // let number = if condition { 5 } else { "six" };
+
+    // Loops
+    loop {
+        println!("only once");
+        // loop is infinite by default
+        break;
+    }
+
+    // This example of using break is confusing, but w/e
+    let mut count = 0;
+    // Loops may have names
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                // Unlabeled break will apply to inner loop
+                break;
+            }
+            if count == 2 {
+                // You may break a loop by name
+                // This allows to break the outer loop
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {}", count);
+
+    // Returning values from loops
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("Loop result: {}", result);
+
+    // While loop
+    let mut number = 3;
+    while number != 0 {
+        println!("{}", number);
+        number -= 1;
+    }
+    println!("LIFTOFF!");
+
+    // For loop
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("current element: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!");
+
+    println!("fib(5): {}", fib(5));
+
+    println!("68 F in C: {}", convert_temp(68.0, TemperatureUnit::Farenheit));
+
+    println!("100 C in F: {}", convert_temp(100.0, TemperatureUnit::Celcius));
+}
+
+// Convert between Celcius and Farenheit
+enum TemperatureUnit {
+    Celcius,
+    Farenheit
+}
+fn convert_temp(temp: f64, unit: TemperatureUnit) -> f64 {
+    match unit {
+        TemperatureUnit::Celcius => temp * 9.0/5.0 + 32.0,
+        TemperatureUnit::Farenheit => (temp - 32.0) * 5.0/9.0,
+    }
+}
+
+// Fibonacci
+fn fib(n: i32) -> i32 {
+    if n<=1 {
+        return 1;
+    } else {
+        return n + fib(n-1);
+    }
+}

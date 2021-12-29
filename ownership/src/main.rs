2733429d8780ded3abaaab3e5edca5c013290de8
Kyle Moad <kyle.moad@gmail.com>
2021-12-28 21:18:54 -0600

    part of structs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -88,8 +88,8 @@ fn main() {
 fn takes_ownership(some_string: String) {
     // some_string comes into scope
     println!("{}", some_string);
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
+} // Here, some_string goes out of scope and `drop` is called.
+  // The backing memory is freed.
 
 fn makes_copy(some_integer: i32) {
     // some_integer comes into scope
---
+++ structs/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    println!("user1.email: {}", user1.email);
+    user1.email = String::from("anotheremail@example.com");
+    println!("user1.email: {}", user1.email);
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+    // user1 is now unusable because user1.username has been moved
+    // println!("user2.username: {}", user1.username);
+    println!("user2.username: {}", user2.username);
+
+    // Tuple structs
+    let black = Color(0,0,0);
+    let origin = Point(0,0,0);
+    println!("black.0: {}", black.0);
+    println!("origing.0: {}", origin.0);
+
+    // Unit-Like structs
+    // These ebcome useful later as containers for traits
+    let subject = AlwaysEqual;
+
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    // Note email and username are just raw names
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+struct AlwaysEqual;
